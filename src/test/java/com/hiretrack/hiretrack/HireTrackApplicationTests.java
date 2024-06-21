f3c74b3a4a19bd9e848f24c3b0bd009a3b227aca
alex-nazemi <alexandernazemi@gmail.com>
2024-06-21 23:06:11 +0100

    project setup

---
+++ src/test/java/com/hiretrack/hiretrack/HireTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiretrack.hiretrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
